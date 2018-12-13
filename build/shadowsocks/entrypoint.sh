bc358d35ce430c182f7e627ba2315cd97249e9a6
anhk <anhk_@126.com>
2018-12-13 12:12:44 +0800

    update

---
+++ build/shadowsocks/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$SS_PORT" ]; then
+    echo -e "[Entrypint] \033[32m\$SS_PORT\033[0m Using default Port: \033[31m1445\033[0m"
+    SS_PORT=1445
+fi
+
+if [ -z "$SS_METHOD" ]; then
+    echo -e "[Entrypint] \033[32m\$SS_METHOD\033[0m Using default Method: \033[31m chacha20 \033[0m"
+    SS_METHOD="chacha20"
+fi
+
+if [ -z "$SS_PASSWORD" ]; then
+    SS_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+    echo -e "[Entrypint] \033[32m\$SS_PASSWORD\033[0m Using Random Password: \033[31m $SS_PASSWORD \033[0m"
+fi
+
+if [ -z "$OBFS_OPTS" ]; then
+    OBFS_OPTS=tls
+    echo -e "[Entrypint] \033[32m\$OBFS_OPTS\033[0m Using default obfs-opts: \033[31m tls \033[0m"
+fi
+
+cat > /etc/ssconfig/ss.json << EOF
+{
+    "server": "0.0.0.0",
+    "server_port": $SS_PORT,
+    "password": "$SS_PASSWORD",
+    "method": "$SS_METHOD",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=$OBFS_OPTS",
+    "timeout": 60
+}
+EOF
+
+exec "$@"
--- build/shadowsocks/start.sh
+++
@@ -1,2 +0,0 @@
-docker run -it --restart always -v $(pwd)/ssconfig:/etc/ssconfig -p 1445:1445 --name ss-server -d anhk/ss-server ss-server -c /etc/ssconfig/ss.json -u
-

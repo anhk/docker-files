db0b917ed15a1766a6d50724ffc23fa77dc7aefc
anhongkui <anhongkui@jd.com>
2023-02-28 12:18:45 +0800

    update

--- build/ubuntu/build.sh
+++ build/ubuntu/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 
-docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t ir0cn/ubuntu .
+docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t quay.io/xxfe/ubuntu .
+#docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t ir0cn/ubuntu .

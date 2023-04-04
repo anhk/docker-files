f0cf7ca6917758e7dedc77bfb5c8723ed5bf86e5
anhongkui <anhongkui@jd.com>
2023-04-04 10:58:31 +0800

    update

--- build/kube-apiserver/build.sh
+++ build/kube-apiserver/build.sh
@@ -2,13 +2,13 @@
 
 arch=$(uname -m)
 version=$(date +%Y%m%d%H%M)
-image="kube-apiserver"
+image="ir0cn/kube-apiserver"
 
 if [ "$arch" = "x86_64" ]; then
     arch=amd64
 fi
 
-docker build -f Dockerfile --no-cache \
+docker build -f Dockerfile \
     -t $image:$arch.$version .
 
 docker tag $image:$arch.$version $image:latest

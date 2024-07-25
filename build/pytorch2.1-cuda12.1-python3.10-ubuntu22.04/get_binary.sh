11147fb0c0a9350bac35fc19162b91b3aad32772
anhongkui <anhongkui@jd.com>
2024-07-25 13:35:29 +0000

    update

---
+++ build/pytorch2.1-cuda12.1-python3.10-ubuntu22.04/get_binary.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://mirror.ghproxy.com/https://github.com/coder/code-server/releases/download/v4.91.1/code-server_4.91.1_amd64.deb \
+	-O code-server_4.91.1_amd64.deb
+
+#wget https://sourceforge.net/projects/code-server.mirror/files/v4.91.1/code-server_4.91.1_amd64.deb/download \
+#	-O code-server_4.91.1_amd64.deb
